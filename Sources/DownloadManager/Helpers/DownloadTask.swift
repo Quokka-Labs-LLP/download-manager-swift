8baab0b14671babbb21ef8f31cf999fde6e53fa9
Abhishek Pandey <abhishek.pandey@quokkalabs.com>
2023-09-08 17:17:12 +0530

    [REFACTOR]: Update on trigger progress notification

--- Sources/DownloadManager/Helpers/DownloadTask.swift
+++ Sources/DownloadManager/Helpers/DownloadTask.swift
@@ -123,9 +123,8 @@ extension DownloadTask {
     
     func triggerProgressNotification(progress: Float) {
         let content = UNMutableNotificationContent()
-        let mediaName = mediaURL.components(separatedBy: "/").last ?? ""
-        content.title = "\(mediaName)"
-        content.body = String(format: "%.1f%% complete", progress * 100)
+        content.title = "Download Progress"
+        content.userInfo = ["progress": "\(progress)"]
         
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
         let request = UNNotificationRequest(identifier: "downloadProgress", content: content, trigger: trigger)
