e0bb3d14bae94a3ce8cbeea3f5d3e2a2df7f3b64
Abhishek Pandey <abhishek.pandey@quokkalabs.com>
2023-09-14 12:16:03 +0530

    [REFACTOR]:  Add public access modifier

--- Sources/DownloadManager/Extension/Double+Extension.swift
+++ Sources/DownloadManager/Extension/Double+Extension.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Double {
     
     //MARK: getTime
-    func getSecondInTime() -> (Int, Int) {
+   public func getSecondInTime() -> (Int, Int) {
         let minutes = Int(self) / 60
         let reminderSeconds = Int(self) % 60
         return(minutes, reminderSeconds)
